9493a5e1c683c7d6feba466175c3b955b6c21da1
yassineaboub <yassineaboub@live.fr>
2020-03-03 21:03:39 +0000

    6-states.sql

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,3 +1,3 @@
 -- script that creates the database hbtn_0d_usa and the table states
 CREATE DATA BASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS [hbtn_0d_usa.]states(id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY, name VARCHAR(256));
