c31c2ba0899db6cb57df25f84f7541cf22da1284
yassineaboub <yassineaboub@live.fr>
2020-03-04 09:04:13 +0000

    9-full_creation.sql

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,8 @@
+-- script that creates a table second_table in the database hbtn_0c_0
+CREATE TABLE IF NOT EXISTS hbtn_0c_0.second_table (id INT, name VARCHAR(256), score INT);
+INSERT INTO hbtn_0c_0.second_table(id, name, score)
+VALUES
+(1, 'John', 10),
+(2, 'Alex', 3),
+(3, 'Bob', 14),
+(4, 'George', 8);
