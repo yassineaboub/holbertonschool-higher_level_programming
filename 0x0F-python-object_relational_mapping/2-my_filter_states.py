20d9ff2c3e2efc4af4078048fc142a11d7328070
yassineaboub <yassineaboub@live.fr>
2020-03-29 22:40:29 +0000

    2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""script that lists all states with a name starting
-with N from the database"""
+"""script that takes in an argument and displays all values"""
 import MySQLdb
 from sys import argv
 if __name__ == "__main__":
@@ -8,7 +7,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306,
                          user=argv[1], passwd=argv[2], db=argv[3])
     c = db.cursor()
-    c.execute("""SELECT id, name FROM states WHERE name LIKE '{}' ORDER
+    c.execute("""SELECT id, name FROM states WHERE name '{}' ORDER
               BY states.id ASC""".format(argv[4]))
     res = c.fetchall()
     for row in res:
