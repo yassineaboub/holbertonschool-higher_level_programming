296756d7ceda3e218e96e217f42a4ec7bba75edf
yassineaboub <yassineaboub@live.fr>
2020-04-05 16:38:56 +0000

    3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,1 +1,8 @@
-#!/usr/bin/pyyhon3
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    n = len(sys.argv)
+    res = 0
+    for x in range(1, n):
+        res += int(sys.argv[x])
+    print('{}'.format(res))
