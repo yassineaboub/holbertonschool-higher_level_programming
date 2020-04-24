664e8fa1267a7ed4f6695a8f5b7e4a2849c6329f
yassineaboub <yassineaboub@live.fr>
2020-04-24 22:19:05 +0000

    first

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+
+    if len(argv) > 1:
+        q = argv[1]
+    else:
+        q = ""
+    try:
+        data = requests.post("http://0.0.0.0:5000/search_user", data={"q": q}).json()
+        if data:
+            print("[{}] {}".format(data['id'], data['name']))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
