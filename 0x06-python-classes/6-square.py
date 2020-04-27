e81273415f74bd5ae4bf1253293c10d481005316
yassineaboub <yassineaboub@live.fr>
2020-04-27 16:00:57 +0000

    6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -33,7 +33,7 @@ class Square:
         return self.__size * self.__size
 
     def my_print(self):
-        if self.__size != 0:
+        if self.__size == 0:
             print()
         else:
             for n in range(self.position[1]):
