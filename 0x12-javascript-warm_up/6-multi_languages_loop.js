19ad89d04c2d33991ba389117cf1216420071452
Mohamed Yassine <yassineaboub@live.fr>
2020-05-05 23:04:06 +0000

    6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const array = ["C is fun", "Python is cool", "Javascript is amazing"];
+const array = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 for (const i in array) {
   console.log(array[i]);
 }
