875ccbb5aab91f4331bc94ac165c5daaeeae1860
Mohamed Yassine <yassineaboub@live.fr>
2020-05-11 21:42:18 +0000

    3-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let r = 0;
+    for (r = 0; r < this.height; r++) {
+      console.log('X'.repeat(this.height))
+    }
+  }
+}
+module.exports = Rectangle;
