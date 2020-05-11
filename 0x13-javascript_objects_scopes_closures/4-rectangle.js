cd11c191e0cb1043528ba2fba2ab20259c3facb2
Mohamed Yassine <yassineaboub@live.fr>
2020-05-11 23:05:29 +0000

    4-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let r = 0;
+    for (r = 0; r < this.height; r++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    var temp;
+    temp = this.width
+    this.width = this.height
+    this.height = temp
+  }
+
+  double () {
+    this.height = this.height * 2;
+    this.width = this.width * 2;
+  }
+}
+module.exports = Rectangle;
