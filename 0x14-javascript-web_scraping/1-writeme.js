4f010a7cf489d7a867645c4566d99055e21da02d
Mohamed Yassine <yassineaboub@live.fr>
2020-05-12 20:13:08 +0000

    script that writes a string to a file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-fs.writeFile(process.argv[2], process.argv[3], 'utf-8', function(err) {
+fs.writeFile(process.argv[2], process.argv[3], 'utf-8', function (err) {
   if (err) {
     console.log(err);
   }
