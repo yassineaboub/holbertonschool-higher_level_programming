10f7d3ebce75397bc67a9375b669de2fb9ca2c26
Mohamed Yassine <yassineaboub@live.fr>
2020-05-12 22:07:30 +0000

    get the contents of a webpage and stores it in a file

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const request = require('request');
+const fs = require('fs');
+
+request(process.argv[2], function (err, res, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    fs.writeFile(process.argv[3], body, 'utf-8', (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+  }
+});
