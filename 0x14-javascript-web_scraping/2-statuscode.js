b87281c72dcfc377c8a830abee8fa17dd18dcaab
Mohamed Yassine <yassineaboub@live.fr>
2020-05-12 20:40:40 +0000

    status code of a GET request

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,7 +2,9 @@
 const request = require('request');
 
 request(process.argv[2], function (err, res, body) {
-  if (err) return console.log('Error: ', err);
+  if (err) {
+    return console.log(err);
+}
   console.log('code: %d', res.statusCode);
 
 });
