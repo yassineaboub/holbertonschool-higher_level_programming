a5fff6e4e499d753b14505fbd6eb441645885c77
Mohamed Yassine <yassineaboub@live.fr>
2020-05-12 20:54:51 +0000

    2-statuscode.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,8 +3,8 @@ const request = require('request');
 
 request(process.argv[2], function (err, res, body) {
   if (err) {
-    return console.log(err);
+    console.log(err);
+  } else {
+    console.log('code: %d', res.statusCode);
 }
-  console.log('code: %d', res.statusCode);
-
 });
