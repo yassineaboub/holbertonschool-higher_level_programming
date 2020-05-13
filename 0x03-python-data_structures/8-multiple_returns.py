8150b6bfe0c7dba7c0e703afff27ab2a8439b0e4
Mohamed Yassine <yassineaboub@live.fr>
2020-05-13 11:00:51 +0000

    8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,6 +2,6 @@
 def multiple_returns(sentence):
     first_char = sentence[0]
     if len(sentence) == 0:
-        return None
+        return first_char is None
     else:
         return (len(sentence), first_char)
